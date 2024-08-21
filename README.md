9da6bec378c9630d327fcaf03b6659425f5873af
baxtian <bastian.contact+github@proton.me>
2024-08-21 11:54:12 +0200

    Add sorted by date, add all existing folders at startup, remove item when a folder is removed

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 # rss_podcast_server
 RSS podcast server based on [NodeJs](https://nodejs.org/en), [Koa](https://www.npmjs.com/package/koa) and [podcast](https://www.npmjs.com/package/podcast) packages
+
+This project lets you host your own podcast. It creates an RSS feed that can be added to podcast applications and websites.
+
+Simply create a folder containing a podcast description json and a jpeg image for the logo, and add a new folder containing a description json and an AAC audio file for each new episode.
+
+It automatically sorts episodes by date.
--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const koa = require('koa');
 const serve = require('koa-static');
 const range = require('koa-range');
 
-const domainName = 'contretempsvoyage.fr'
+const domainName = 'YOUR_DOMAIN'
 const url = 'https://' + domainName;
-const staticPodcastPath = path.join(__dirname, 'contre_temps_pod');
+const staticPodcastPath = path.join(__dirname, 'YOUR_PODCAST_FOLDER');
 const podJsonPath = '/podcast.json';
 const podImgPath = '/podcast.jpg';
 
@@ -24,39 +24,81 @@ const podImgPath = '/podcast.jpg';
     https: {
         port: 443, // any port that is open and not already used on your server
         options: {
-        key: fs.readFileSync(path.resolve(process.cwd(), __dirname + '/../../certs/privkey.pem'), 'utf8').toString(),
-        cert: fs.readFileSync(path.resolve(process.cwd(), __dirname + '/../../certs/fullchain.pem'), 'utf8').toString(),
+          key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/YOUR_DOMAIN/privkey.pem'), 'utf8').toString(),
+          cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/YOUR_DOMAIN/fullchain.pem'), 'utf8').toString(),
         },
     },
 };*/
 
+let episodesMap = new Map(); // To keep track of added episodes
 
+/**
+ * @brief Reads a JSON file and parses its content.
+ * @param filePath The path to the JSON file.
+ * @return Parsed JSON object, or null if an error occurs.
+ */
 function readJSONFile(filePath) {
   try {
     const fileContent = fs.readFileSync(filePath, 'utf8');
     return JSON.parse(fileContent);
   } catch (error) {
-    console.error(`Erreur lors de la lecture du fichier JSON ${filePath}:`, error);
+    console.error(`Error reading JSON file ${filePath}:`, error);
     return null;
   }
 }
 
-function addEpisod(jsonFile, audioFile, imageFile) {
-  feed.addItem({
-    title: jsonFile.title,
-    description: jsonFile.description,
-    url: url + '/' + audioFile, // link to the item
-    date: jsonFile.date, // any format that js Date can parse.
-    enclosure: { url: url + '/' + audioFile, file: staticPodcastPath + '/' + audioFile }, // optional enclosure
-    itunesExplicit: false,
-    itunesDuration: jsonFile.duration,
-    itunesImage: url + imageFile
-  });
+/**
+ * @brief Sorts episodes by date in descending order (most recent first).
+ */
+function sortEpisodesByDate() {
+  // Convert the Map to an array and sort it by the date of the episodes
+  const sortedEpisodes = [...episodesMap.entries()].sort(([, a], [, b]) => new Date(b.date) - new Date(a.date));
+  // Rebuild the feed with sorted episodes
+  feed.items = sortedEpisodes.map(([, episode]) => episode);
 }
 
 /**
- * @param {*} folderPath The folder where to check the contents
- * @returns True or false if there is all useful files or not
+ * @brief Adds an episode to the RSS feed.
+ * @param jsonFile The JSON metadata file for the episode.
+ * @param audioFile The audio file associated with the episode.
+ * @param imageFile The image file for the episode (or default podcast image).
+ * @param folderName The name of the folder containing the episode files.
+ */
+function addEpisod(jsonFile, audioFile, imageFile, folderName) {
+  const episode = {
+    title: jsonFile.title,
+    description: jsonFile.description,
+    //url: url + '/' + audioFile, // link to the item
+    date: jsonFile.date, // any format that js Date can parse.
+    enclosure: {
+      url: url + '/' + audioFile,
+      file: staticPodcastPath + '/' + audioFile,
+      type: 'audio/x-m4a'
+    },
+    itunesExplicit: false,
+    itunesDuration: jsonFile.duration,
+    itunesImage: url + imageFile
+  };
+
+  episodesMap.set(folderName, episode);// Store the episode with the folder name
+  sortEpisodesByDate(); // Sort episodes by date after adding
+}
+
+/**
+ * @brief Removes an episode from the RSS feed.
+ * @param folderName The name of the folder containing the episode files.
+ */
+function removeEpisode(folderName) {
+  if (episodesMap.delete(folderName)) {
+      sortEpisodesByDate(); // Re-sort episodes after removal
+      console.log(`Episode removed: ${folderName}`);
+  }
+}
+
+/**
+ * @brief Checks if a folder contains the necessary files for an episode.
+ * @param folderPath The folder where to check the contents.
+ * @return Promise resolving to an object with file names if valid, or false if not.
  */
 async function checkFolderContent(folderPath) {
   return new Promise((resolve, reject) => {
@@ -67,7 +109,7 @@ async function checkFolderContent(folderPath) {
         const m4aFile = files.find(file => file.endsWith('.m4a'));
         const jsonFile = files.find(file => file.endsWith('.json'));
         if (m4aFile && jsonFile) {
-          resolve(true);
+          resolve({ m4aFile, jsonFile });
         } else {
           resolve(false);
         }
@@ -76,16 +118,69 @@ async function checkFolderContent(folderPath) {
   });
 }
 
-// Check the content of the podcast folder
-fs.watch(staticPodcastPath, { recursive: false }, async (eventType, filename) => {
-  if (eventType === 'rename') {
-    // When a change is detected in the public folder
-    const fullPath = path.join(staticPodcastPath, filename);
+/**
+ * @brief Processes a folder to add the episode if valid.
+ * @param folderName The name of the folder containing the episode files.
+ * @param folderPath The full path to the folder.
+ */
+async function processEpisodeFolder(folderName, folderPath) {
+  const isContentValid = await checkFolderContent(folderPath);
+  if (isContentValid) {
+      const { m4aFile, jsonFile } = isContentValid;
+      const episodeJson = readJSONFile(path.join(folderPath, jsonFile));
+      const episodeImagePath = path.join(folderPath, 'episode.jpg');
+      let imageUrl;
+      if (fs.existsSync(episodeImagePath)) {
+          imageUrl = '/' + folderName + '/episode.jpg';
+      } else {
+          imageUrl = podImgPath;
+      }
+      addEpisod(episodeJson, folderName + '/' + m4aFile, imageUrl, folderName);
+      console.log(`Episode added: ${folderName}`);
+  } else {
+      console.log(`Necessary files are not all present in folder ${folderName}`);
+  }
+}
 
+/**
+ * @brief Adds all existing episodes in the podcast directory to the RSS feed on startup.
+ */
+function addExistingEpisodes() {
+  fs.readdir(staticPodcastPath, (err, folders) => {
+      if (err) {
+          console.error('Error reading podcast directory:', err);
+          return;
+      }
+      folders.forEach(async (folder) => {
+          const folderPath = path.join(staticPodcastPath, folder);
+          fs.stat(folderPath, async (err, stats) => {
+              if (err) {
+                  console.error(`Error checking folder ${folder}`, err);
+                  return;
+              }
+              if (stats.isDirectory()) {
+                  await processEpisodeFolder(folder, folderPath);
+              }
+          });
+      });
+  });
+}
+
+// Call the function at startup to add existing episodes
+addExistingEpisodes();
+
+/**
+ * @brief Watches the podcast folder for new or deleted folders and updates the RSS feed accordingly.
+ */
+fs.watch(staticPodcastPath, { recursive: false }, async (eventType, filename) => {
+  const fullPath = path.join(staticPodcastPath, filename);
+
+  if (eventType === 'rename') {
     // Check if the modified item is a folder
     fs.stat(fullPath, (err, stats) => {
       if (err) {
         if (err.code === 'ENOENT') {
+          removeEpisode(filename);
           return;
         } else {
           console.error(`Error checking element type ${filename}`, err);
@@ -99,23 +194,8 @@ fs.watch(staticPodcastPath, { recursive: false }, async (eventType, filename) =>
 
         // Check periodically every second for up to 30 seconds
         const interval = setInterval(async () => {
-          const isContentValid = await checkFolderContent(folderPath);
-          if (isContentValid) {
-            console.log(`The necessary files are located in the ${filename}`);
-            // Add episod image or podcast image
-            const episodeImagePath = path.join(folderPath, 'episod.jpg');
-            let imageUrl;
-            if (fs.existsSync(episodeImagePath)) {
-              imageUrl = '/' + filename + '/episod.jpg';
-            } else {
-              imageUrl = podImgPath;
-            }
-            // Update RSS feed here
-            addEpisod(readJSONFile(folderPath + '/episod.json'), filename + '/episod.m4a', imageUrl);
-            clearInterval(interval); // Stop periodic verification
-          } else {
-            console.log(`Not all the necessary files are in ${filename}`);
-          }
+          await processEpisodeFolder(filename, fullPath);
+          clearInterval(interval); // Stop periodic verification
         }, 1000); // Check every second
 
         // Stop verification after 30 seconds
@@ -126,7 +206,8 @@ fs.watch(staticPodcastPath, { recursive: false }, async (eventType, filename) =>
     });
   } else if (eventType === 'unlink') {
     // When an item is deleted from the podcast folder
-    console.log(`Deleted item : ${filename}`);
+    console.log(`Item deleted : ${filename}`);
+    removeEpisode(filename);
   }
 });
 
@@ -159,6 +240,7 @@ const feed = new Podcast({
 
 const server = new koa();
 
+// Add HTTP Range request
 server.use(range);
 
 // Add the content of podcast folder available to the webserver
@@ -182,7 +264,6 @@ server.use(async (ctx, next) => {
 
 // If access to the page is not authorized
 server.use(async (ctx) => {
-  ctx.status = 404;
   ctx.body = "C'est pas là !";
 })
 
